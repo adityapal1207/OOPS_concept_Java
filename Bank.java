12ff679c4b36a29138bc1333bef373d0b2279f13
adityapal1207 <87771100+adityapal1207@users.noreply.github.com>
2023-07-01 01:34:20 +0530

    Add files via upload
    Encapsulation and packages concepts

---
+++ Bank.java
@@ -0,0 +1,20 @@
+package bank;
+class Account{
+    public String name;
+    public String email;
+    public String getPassword(){
+        setPassword(randomPass){  //just for example
+          return this.password;
+        }
+    }
+}
+public class Bank {
+    public static void main(String args[]){
+        Account account2= new Account();
+        account2.name="Customer2";
+        account2.email="adityapal1207@gmail.com";
+        account2.getPassword();
+        System.out.println(account2.getPassword());
+
+    }
+}
---
+++ Bank2.java
@@ -0,0 +1,9 @@
+import bank;
+public class Bank2 {
+    public static void main(String args[]){
+        bank.Account account1=new bank.Account();
+        account1.name="customer1";
+        
+    }
+    
+}
